59087ca3b4b0fee2928b89e87a0529e902cdc0c3
romeo gomez <romegomez29@gmail.com>
2026-08-12 22:52:10 -0300

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -218,7 +218,7 @@ export default function App() {
           </div>
         )}
 
-        <main className="flex-1 overflow-y-auto p-4 md:p-8">
+        <main className="flex-1 overflow-y-auto p-4 pb-20 md:p-8">
           <div className="mx-auto max-w-7xl">
             {renderCurrentView()}
           </div>
